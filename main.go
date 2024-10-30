25fd8df7afed40e6648831872dd1c7347417d97e
Thomas Rommel <thomasrommel89@gmail.com>
2024-10-30 14:09:35 +0100

    initial commit

--- main.go
+++ main.go
@@ -13,8 +13,8 @@ import (
 // @description The Pepper Analytics AI platform is a comprehensive system designed to manage and analyze data related to plant breeding projects. It provides a RESTful API for various operations, including managing species, varieties, plants, fertilizers, observations, seed banks, breeding projects, and notes. The platform uses the Gin framework for routing and middleware, and it integrates Swagger for API documentation. The system ensures data integrity through transaction middleware and supports CRUD operations for all major entities.
 // @host localhost:8080
 // @BasePath /api/v1
-// @contact.name: Thomas Rommel
-// @contact.email: thomasrommel89@gmail.com
+// @contact.name: -
+// @contact.email: -
 // @license.name: MIT
 // @license.url: https://opensource.org/licenses/MIT
 // @schemes http https
