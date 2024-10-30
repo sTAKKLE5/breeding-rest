a1c948373e921522fe2c6ab09aa6a67bca072cb1
Thomas Rommel <thomasrommel89@gmail.com>
2024-10-30 14:26:49 +0100

    initial commit

--- docs/docs.go
+++ docs/docs.go
@@ -157,7 +157,7 @@ var SwaggerInfo = &swag.Spec{
 	BasePath:         "/api/v1",
 	Schemes:          []string{"http", "https"},
 	Title:            "Pepper Analytics API",
-	Description:      "The Pepper Analytics AI platform is a comprehensive system designed to manage and analyze data related to plant breeding projects. It provides a RESTful API for various operations, including managing species, varieties, plants, fertilizers, observations, seed banks, breeding projects, and notes. The platform uses the Gin framework for routing and middleware, and it integrates Swagger for API documentation. The system ensures data integrity through transaction middleware and supports CRUD operations for all major entities.",
+	Description:      "A Go REST API that calculates F2 generation probabilities in plant breeding genetics. Calculate inheritance patterns and expected phenotype distributions based on parent plant genotypes. Features Swagger documentation and handles multiple genetic traits with dominant/recessive expressions.",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
 	LeftDelim:        "{{",
