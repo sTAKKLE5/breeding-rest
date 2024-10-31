00a5dcdd7dd73d774be119dc576b8e1b5500db58
Thomas Rommel <thomasrommel89@gmail.com>
2024-10-31 11:27:32 +0100

    index route including tailwind added

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
 	"os"
 )
 
@@ -20,6 +21,13 @@ func SetupRouter() *gin.Engine {
 	}
 
 	router := gin.Default()
+
+	router.LoadHTMLGlob("templates/**/*")
+	router.Static("/css", "./static/css")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 
 	v1 := router.Group("/api/v1")
 	v1.POST("/trait-probability", handlers.CalculateTraitProbability)
